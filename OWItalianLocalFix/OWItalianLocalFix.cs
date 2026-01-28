83ffa9dac7e59c5c6d7f1d8cf4c55a9f8e0346f5
435THz <delbovemarie@gmail.com>
2026-01-28 17:06:03 +0100

    First commit

---
+++ OWItalianLocalFix/OWItalianLocalFix.cs
@@ -0,0 +1,41 @@
+﻿using HarmonyLib;
+using OWML.Common;
+using OWML.ModHelper;
+using System.Reflection;
+
+namespace OWItalianLocalFix;
+
+public class OWItalianLocalFix : ModBehaviour
+{
+	public static OWItalianLocalFix Instance;
+
+	public void Awake()
+	{
+		Instance = this;
+		// You won't be able to access OWML's mod helper in Awake.
+		// So you probably don't want to do anything here.
+		// Use Start() instead.
+	}
+
+	public void Start()
+	{
+		// Starting here, you'll have access to OWML's mod helper.
+		ModHelper.Console.WriteLine($"My mod {nameof(OWItalianLocalFix)} is loaded!", MessageType.Success);
+
+		new Harmony("MistressNebula.OWItalianLocalFix").PatchAll(Assembly.GetExecutingAssembly());
+
+        var api = ModHelper.Interaction.TryGetModApi<ILocalizationAPI>("xen.LocalizationUtility");
+        api.RegisterLanguage(this, "Italiano (fix)", "assets/Translation.xml");
+
+		// Example of accessing game code.
+		OnCompleteSceneLoad(OWScene.TitleScreen, OWScene.TitleScreen); // We start on title screen
+		LoadManager.OnCompleteSceneLoad += OnCompleteSceneLoad;
+	}
+
+	public void OnCompleteSceneLoad(OWScene previousScene, OWScene newScene)
+	{
+		if (newScene != OWScene.SolarSystem) return;
+		ModHelper.Console.WriteLine("Loaded into solar system!", MessageType.Success);
+	}
+}
+
---
+++ README.md
@@ -0,0 +1,13 @@
+# Outer Wilds Italian Localization Fix
+
+This mod contains a fixed italian localization table, which is added to the game as a new language option.
+
+The italian translation isn't too bad usually, but a few dialogues contain some errors that are inexcusable, while others can make the process of understanding the game's story much more difficult than it needs to be, or even render some sentences pretty much nonsensical.
+This mod aims to fix all of that.
+
+### IT
+Questa mod aggiunge alle lingue di gioco una tabella di localizzazione migliorata per la lingua italiana, attivabile nelle opzioni.
+
+La traduzione italiana normalmente non è male, ma ci sono alcuni dialoghi che contengono errori inaccettabili, rendono la storia più difficile da comprensere senza un vero motivo, oppure sono completamente prive di senso. Questa mod cerca di risolvere tutto questo.
+
+**Da notare**: Questa mod cambia anche il nome di alcuni luoghi o in maniera tale che siano più in linea con la traduzione originale (ad esempio *"Giant's Deep"* -> *"Abisso del Gigante"*), oppure per risolvere degli errori di traduzione (ad esempio, *"Timber Hearth"* -> *"Tepore Legnoso"*). Non si tratta di cambiamenti che impattano sulla run, ma è da tenere a mente quando si parla con altri giocatori.
